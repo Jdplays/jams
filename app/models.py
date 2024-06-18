02819a36f58ac59bef26318e5cc001c0d55da2c5
James Dougan <>
2024-06-18 17:53:30 +0100

    update password hash type

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='bcrypt')
+        self.password_hash = generate_password_hash(password)
 
     
     def check_password(self, password):
