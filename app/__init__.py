00cb7a678a7b7b21362a7a9a057228baf9c9c365
James Dougan <>
2024-06-18 16:43:40 +0100

    add db migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 
@@ -14,6 +16,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://jams:jams@db:5432/jams-main')
 
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     from . import routes, models
