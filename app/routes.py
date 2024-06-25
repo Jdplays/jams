7ca15480920b0aa1d3734ff28cb8ae53a287cf26
James Dougan <>
2024-06-25 12:52:41 +0100

    fix import

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_security import roles_required
-from . import db
+from .extensions import db
 from .models import User
 from .forms import LoginForm, RegisterForm
 
