e5d01d5ebc2e8ce22a386f78669d2e5fce76d98b
James Dougan <>
2024-06-25 19:37:15 +0100

    Change form to custom login form so it doesnt interfer with flask security too login form

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class CustomLoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_security import roles_required, login_required, login_user, logout_user, current_user
 from .extensions import db
 from .models import User
-from .forms import LoginForm, RegisterForm
+from .forms import CustomLoginForm, RegisterForm
 
 bp = Blueprint('main', __name__)
 
@@ -10,7 +10,7 @@ bp = Blueprint('main', __name__)
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
-    form = LoginForm()
+    form = CustomLoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
