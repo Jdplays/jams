39ccf27763a6ad7877de51db9b27d5f26978ed60
James Dougan <>
2024-06-26 19:29:41 +0100

    Run db migration and db seeding in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Change directory to where the Flask application resides
+cd /app/flask_app
+
+# Check if the migrations directory exists, if not, initialize it
+if [ ! -d "migrations" ]; then
+  flask db init
+fi
+
 # Apply database migrations
-flask db init  # Initialize migrations directory (only once)
-flask db migrate -m "Initial migration"
+flask db migrate -m "Automatic migration"
 flask db upgrade
+
+# Seed the database
+flask shell <<EOF
+from app import create_app, seed_database
+app = create_app()
+seed_database(app)
+EOF
 
 # Start Gunicorn
 exec gunicorn -b 0.0.0.0:5000 "app:create_app()"
