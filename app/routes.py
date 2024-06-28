fa2b37df59daa19709ac1419aff722f1939f4e4e
James Dougan <>
2024-06-28 23:15:04 +0100

    Add workshop Catalog page with ability to view add edit and archive

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 from flask_security import roles_required, login_required, current_user
-from .models import User, Role
+from .models import db, User, Role, Workshop
 
 bp = Blueprint('main', __name__)
 
@@ -26,9 +26,15 @@ def volunteer():
 def user_management():
     return render_template('admin/user_management.html')
 
+@bp.route('/management/workshop_catalog')
+@login_required
+@roles_required('Volunteer')
+def workshop_catalog():
+    return render_template('management/workshop_catalog.html')
+
 
 ## API for AJAX requests ##
-@bp.route('/api/admin/get_user_management_table', methods=['get'])
+@bp.route('/api/admin/get_user_management_table', methods=['GET'])
 @login_required
 @roles_required('Admin')
 def get_user_management_table():
@@ -49,4 +55,125 @@ def get_user_management_table():
     return jsonify({
         'all_roles': all_roles,
         'users': users_data_list
+    })
+
+@bp.route('/api/management/get_workshop_catalog_table', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_workshop_catalog_table():
+    workshops = Workshop.query.all()
+    workshops_data_list = []
+    for workshop in workshops:
+        workshops_data_list.append({
+            'id': workshop.id,
+            'name': workshop.name,
+            'description': workshop.description,
+            'min_volunteers': workshop.min_volunteers,
+            'active': workshop.active
+        })
+    return jsonify({
+        'workshops': workshops_data_list
+    })
+
+@bp.route('/api/management/get_workshop_details/<int:workshop_id>', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_workshop(workshop_id):
+    workshop = Workshop.query.filter_by(id=workshop_id).first()
+    return jsonify({
+        'id': workshop.id,
+        'name': workshop.name,
+        'description': workshop.description,
+        'min_volunteers': workshop.min_volunteers,
+        'active': workshop.active
+    })
+
+@bp.route('/api/management/add_workshop', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def add_workshop():
+    try:
+        name = request.form.get('name')
+        description = request.form.get('description')
+        min_volunteers = request.form.get('min_volunteers')
+        new_workshop = Workshop(name=name, description=description, min_volunteers=min_volunteers)
+        db.session.add(new_workshop)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to add new workshop'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'New workshop has been added to the system'
+    })
+
+@bp.route('/api/management/edit_workshop', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def edit_workshop():
+    try:
+        data = request.get_json()
+        workshop_id = data.get('workshop_id')
+        name = data.get('name')
+        description = data.get('description')
+        min_volunteers = data.get('min_volunteers')
+        workshop = Workshop.query.filter_by(id=workshop_id).first()
+        workshop.name = name
+        workshop.description = description
+        workshop.min_volunteers = min_volunteers
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to edit workshop'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Workshop has been successfuly edited'
+    })
+
+@bp.route('/api/management/archive_workshop', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def archive_workshop():
+    try:
+        data = request.get_json()
+        workshop_id = data.get('workshop_id')
+        workshop = Workshop.query.filter_by(id=workshop_id).first()
+        workshop.archive()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to archive workshop'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Workshop has been archived'
+    })
+
+@bp.route('/api/management/activate_workshop', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def activate_workshop():
+    try:
+        data = request.get_json()
+        workshop_id = data.get('workshop_id')
+        workshop = Workshop.query.filter_by(id=workshop_id).first()
+        workshop.activate()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to activate workshop'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Workshop has been activated'
     })
---
+++ app/static/js/management/workshop_catalog.js
@@ -0,0 +1,149 @@
+function fetchDataAndPopulateWorkshopcatalogTable() {
+        $.ajax({
+            url: '/api/management/get_workshop_catalog_table',
+            type: 'GET',
+            success: function(response) {
+                workshops = response.workshops;
+
+                $('#workshop-table tbody').empty();
+
+                workshops.forEach(function(workshop) {
+
+                    
+                    actionButtonHtml = ''
+
+                    if (workshop.active){
+                        actionButtonHtml = '<button onclick="prepEditWorkshopForm(' + workshop.id + ')">Edit</button>' + 
+                        '<button onclick="archiveWorkshop(' + workshop.id + ')">Archive</button>'
+                    }
+                    else {
+                        actionButtonHtml = '<button onclick="prepEditWorkshopForm(' + workshop.id + ')" disabled>Edit</button>' + 
+                        '<button onclick="activateWorkshop(' + workshop.id + ')">Activate</button>'
+                    }
+
+                    var row = '<tr>' + 
+                    '<td>' + workshop.name + '</td>' +
+                    '<td>' + workshop.description + '</td>' +
+                    '<td>' + workshop.min_volunteers + '</td>' +
+                    '<td>' + workshop.active + '</td>' +
+                    '<td>' + actionButtonHtml + '</td>' +
+                    '</tr>';
+
+                    $('#workshop-table').append(row);
+                });
+            },
+            error: function(error) {
+                console.log('Error fetching data:', error);
+            }
+        });
+}
+
+function addNewWorkshopFromForm(event) {
+    event.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/management/add_workshop',
+        data: $('#add-workshop-form').serialize(),
+        success: function(response) {
+            if (response.status === 'success') {
+                fetchDataAndPopulateWorkshopcatalogTable();
+            }
+
+            document.getElementById('workshop-request-response').innerHTML = response.message
+        }
+    });
+}
+
+function prepEditWorkshopForm(workshopID) {
+    document.getElementById('edit-workshop-block').style.display = 'block'
+
+    $.ajax({
+        url: '/api/management/get_workshop_details/' + workshopID,
+        type: 'GET',
+        success: function(response) {
+            workshopID = response.id;
+            workshopName = response.name;
+            workshopDescription = response.description;
+            workshopMinVolunteers = response.min_volunteers;
+
+            document.getElementById('edit-workshop-id').value = workshopID
+            document.getElementById('edit-workshop-name').value = workshopName
+            document.getElementById('edit-workshop-description').innerText = workshopDescription
+            document.getElementById('edit-workshop-min_volunteers').value = workshopMinVolunteers
+        }
+    });
+
+}
+
+function editWorkshopFromForm(event) {
+    event.preventDefault();
+
+    const data = {
+        'workshop_id': document.getElementById('edit-workshop-id').value,
+        'name': document.getElementById('edit-workshop-name').value,
+        'description': document.getElementById('edit-workshop-description').value,
+        'min_volunteers': document.getElementById('edit-workshop-min_volunteers').value,
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/management/edit_workshop',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function(response) {
+            if (response.status === 'success') {
+                fetchDataAndPopulateWorkshopcatalogTable();
+                document.getElementById('edit-workshop-block').style.display = 'none'
+            }
+
+            document.getElementById('workshop-request-response').innerHTML = response.message
+        }
+    });
+}
+
+function archiveWorkshop(workshopID) {
+    const data = {
+        'workshop_id': workshopID
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/api/management/archive_workshop',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function(response) {
+            if (response.status === 'success') {
+                fetchDataAndPopulateWorkshopcatalogTable();
+            }
+
+            document.getElementById('workshop-request-response').innerHTML = response.message
+        }
+    });
+}
+
+function activateWorkshop(workshopID) {
+    const data = {
+        'workshop_id': workshopID
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/api/management/activate_workshop',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function(response) {
+            if (response.status === 'success') {
+                fetchDataAndPopulateWorkshopcatalogTable();
+            }
+
+            document.getElementById('workshop-request-response').innerHTML = response.message
+        }
+    });
+}
+
+// Event listeners
+document.addEventListener("DOMContentLoaded", fetchDataAndPopulateWorkshopcatalogTable);
+document.getElementById('add-workshop-form').addEventListener('submit', addNewWorkshopFromForm);
+document.getElementById('edit-workshop-form').addEventListener('submit', editWorkshopFromForm);
+document.getElementById('edit-workshop-form').addEventListener('reset', function() {
+    document.getElementById('edit-workshop-block').style.display = 'none'
+});
