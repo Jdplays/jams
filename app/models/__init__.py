f4d857281649c9adcd811da0ba01d48a2bd96d6b
James Dougan <jdplaysgames200@gmail.com>
2024-07-02 21:17:08 +0100

    Add location and timeslot db models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from ..extensions import db
 
 from .auth import User, Role
 from .management import Workshop
+from .event import Event, Location, Timeslot, Session
 
-__all__ = ['User', 'Role', 'Workshop']
+__all__ = ['User', 'Role', 'Workshop', 'Event', 'Location', 'Timeslot', 'Session']
