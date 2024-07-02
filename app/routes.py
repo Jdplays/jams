48bc3227d00db3dbcd8aaec6ef01a27cea706993
James Dougan <jdplaysgames200@gmail.com>
2024-07-02 21:17:58 +0100

    Add location and timeslot routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+import json
 from flask import Blueprint, render_template, jsonify, request
 from flask_security import roles_required, login_required, current_user
-from .models import db, User, Role, Workshop
+from .models import db, User, Role, Workshop, Location, Timeslot
 
 bp = Blueprint('main', __name__)
 
@@ -31,6 +32,12 @@ def user_management():
 @roles_required('Volunteer')
 def workshop_catalog():
     return render_template('management/workshop_catalog.html')
+
+@bp.route('/management/locations_timeslots')
+@login_required
+@roles_required('Volunteer')
+def locations_timeslots():
+    return render_template('management/locations_timeslots.html')
 
 
 ## API for AJAX requests ##
@@ -220,4 +227,239 @@ def activate_workshop():
     return jsonify({
         'status': 'success',
         'message': 'Workshop has been activated'
+    })
+
+@bp.route('/api/management/get_locations_table', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_locations_table():
+    locations = Location.query.all()
+    locations_data_list = []
+    for location in locations:
+        locations_data_list.append({
+            'id': location.id,
+            'name': location.name,
+            'active': location.active
+        })
+    return jsonify({
+        'locations': locations_data_list
+    })
+
+@bp.route('/api/management/get_location_details/<int:location_id>', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_location(location_id):
+    location = Location.query.filter_by(id=location_id).first()
+    return jsonify({
+        'id': location.id,
+        'name': location.name,
+        'active': location.active
+    })
+
+@bp.route('/api/management/add_location', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def add_location():
+    try:
+        name = request.form.get('name')
+        new_location = Location(name=name)
+        db.session.add(new_location)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to add new location'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'New location has been added to the system'
+    })
+
+@bp.route('/api/management/edit_location', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def edit_location():
+    try:
+        data = request.get_json()
+        location_id = data.get('location_id')
+        name = data.get('name')
+        location = Location.query.filter_by(id=location_id).first()
+        location.name = name
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to edit location'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Location has been successfuly edited'
+    })
+
+@bp.route('/api/management/archive_location', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def archive_location():
+    try:
+        data = request.get_json()
+        location_id = data.get('location_id')
+        location = Location.query.filter_by(id=location_id).first()
+        location.archive()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to archive location'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Location has been archived'
+    })
+
+@bp.route('/api/management/activate_location', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def activate_location():
+    try:
+        data = request.get_json()
+        location_id = data.get('location_id')
+        location = Location.query.filter_by(id=location_id).first()
+        location.activate()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to activate location'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Location has been activated'
+    })
+
+## Timeslots
+
+
+@bp.route('/api/management/get_timeslots_table', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_timeslots_table():
+    timeslots = Timeslot.query.all()
+    timeslots_data_list = []
+    for timeslot in timeslots:
+        timeslots_data_list.append({
+            'id': timeslot.id,
+            'name': timeslot.name,
+            'start': str(timeslot.start),
+            'end': str(timeslot.end),
+            'active': timeslot.active
+        })
+    return jsonify({
+        'timeslots': timeslots_data_list
+    })
+
+@bp.route('/api/management/get_timeslot_details/<int:timeslot_id>', methods=['GET'])
+@login_required
+@roles_required('Volunteer')
+def get_timeslot(timeslot_id):
+    timeslot = Timeslot.query.filter_by(id=timeslot_id).first()
+    return jsonify({
+        'id': timeslot.id,
+        'name': timeslot.name,
+        'start': str(timeslot.start),
+        'end': str(timeslot.end),
+        'active': timeslot.active
+    })
+
+@bp.route('/api/management/add_timeslot', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def add_timeslot():
+    try:
+        name = request.form.get('name')
+        start = request.form.get('start_time')
+        end = request.form.get('end_time')
+        new_timeslot = Timeslot(name=name, start=start, end=end)
+        db.session.add(new_timeslot)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to add new timeslot'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'New timeslot has been added to the system'
+    })
+
+@bp.route('/api/management/edit_timeslot', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def edit_timeslot():
+    try:
+        data = request.get_json()
+        timeslot_id = data.get('timeslot_id')
+        name = data.get('name')
+        start = data.get('start_time')
+        end = data.get('end_time')
+        timeslot = Timeslot.query.filter_by(id=timeslot_id).first()
+        timeslot.name = name
+        timeslot.start = start
+        timeslot.end = end
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to edit timeslot'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Timeslot has been successfuly edited'
+    })
+
+@bp.route('/api/management/archive_timeslot', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def archive_timeslot():
+    try:
+        data = request.get_json()
+        timeslot_id = data.get('timeslot_id')
+        timeslot = Timeslot.query.filter_by(id=timeslot_id).first()
+        timeslot.archive()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to archive timeslot'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Timeslot has been archived'
+    })
+
+@bp.route('/api/management/activate_timeslot', methods=['POST'])
+@login_required
+@roles_required('Volunteer')
+def activate_timeslot():
+    try:
+        data = request.get_json()
+        timeslot_id = data.get('timeslot_id')
+        timeslot = Timeslot.query.filter_by(id=timeslot_id).first()
+        timeslot.activate()
+        db.session.commit()
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': 'An Error occured when trying to activate timeslot'
+        })
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Timeslot has been activated'
     })
