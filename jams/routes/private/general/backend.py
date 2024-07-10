77584fb8cb729c9d9ac1d043c2f8ece646bbc1bd
James Dougan <jdplaysgames200@gmail.com>
2024-07-10 20:01:58 +0100

    add current user id route to general

--- jams/routes/private/general/backend.py
+++ jams/routes/private/general/backend.py
@@ -1,6 +1,13 @@
 # Backend is just for serving data to javascript
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask_security import login_required, current_user
 
 bp = Blueprint('backend', __name__, url_prefix='/backend')
 
 # URL PREFIX = /backend
+
+@bp.route('/get_current_user_id', methods=['GET'])
+@login_required
+def get_current_user_id():
+    if current_user.is_authenticated:
+        return jsonify({'current_user_id': current_user.id})
