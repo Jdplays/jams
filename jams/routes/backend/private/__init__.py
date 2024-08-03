56d33573ce301e93eabce3ffa884765114986dbc
James Dougan <jdplaysgames200@gmail.com>
2024-08-03 18:08:55 +0100

    register backend functions

--- jams/routes/backend/private/__init__.py
+++ jams/routes/backend/private/__init__.py
@@ -4,6 +4,7 @@ from .admin import bp as admin_bp
 from .management import bp as management_bp
 from .general import bp as general_bp
 from .volunteer import bp as volunteer_bp
+from .monitoring import bp as monitoring_bp
 
 private_bp = Blueprint('private', __name__)
 
@@ -11,3 +12,4 @@ private_bp.register_blueprint(admin_bp)
 private_bp.register_blueprint(management_bp)
 private_bp.register_blueprint(general_bp)
 private_bp.register_blueprint(volunteer_bp)
+private_bp.register_blueprint(monitoring_bp)
