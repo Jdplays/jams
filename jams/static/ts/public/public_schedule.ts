5bde153da8b9899e7b91c9dd7c58705d486ff63a
James Dougan <jdplaysgames200@gmail.com>
2024-08-22 23:15:49 +0100

    Migrate public schedule

---
+++ jams/static/ts/public/public_schedule.ts
@@ -0,0 +1,16 @@
+import {ScheduleGrid} from '../global/schedule_grid'
+
+const scheduleGridOptions = {
+    eventId: 1,
+    edit: false,
+    autoScale: true,
+    size: 388
+}
+
+const scheduleGrid = new ScheduleGrid('schedule-container', scheduleGridOptions)
+
+document.addEventListener("DOMContentLoaded", async function () {
+    let bodyContainer = document.getElementById('body-container')
+    bodyContainer.classList.remove('container')
+    bodyContainer.classList.add('container-fluid')
+});
