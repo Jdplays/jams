568ae3bb0fe14503551948d2cc43d64b7d798759
James Dougan <jdplaysgames200@gmail.com>
2024-09-09 22:23:52 +0100

    Tidy up old code

--- jams/seeder.py
+++ jams/seeder.py
@@ -13,35 +13,15 @@ def preform_seed():
     seed_locations()
     seed_timeslots()
 
-def seed_roles():
-    roles = ['Attendee', 'Volunteer', 'Trustee', 'Admin']
-    for role_name in roles:
-        role = Role.query.filter_by(name=role_name).first()
-        if not role:
-            new_role = Role(name=role_name)
-            db.session.add(new_role)
-    
-    db.session.commit()
-
 def seed_users():
     # Check if the Admin user already exists
     if not User.query.filter_by(email="admin@test.com").first():
         admin_role = Role.query.filter_by(name="Admin").first()
         if admin_role:
-            user = User(email='admin@test.com', username="AdminAccount", password=hash_password('admin'))
+            user = User(email='admin@test.com', username="AdminAccount", password=hash_password('admin'), user_induction=True)
             user.set_roles_by_name(['Admin', 'Volunteer'])
             user.activate()
             db.session.add(user)
-    
-    # Check if the Volunteer user already exists
-    if not User.query.filter_by(email="volunteer@test.com").first():
-        volunteer_role = Role.query.filter_by(name="Volunteer").first()
-        if volunteer_role:
-            user = User(email='volunteer@test.com', username="VolunteerAccount", password=hash_password('password123'))
-            user.set_roles_by_name(['Volunteer'])
-            user.activate()
-            db.session.add(user)
-    
     db.session.commit()
 
 def seed_difficulty_levels():
