fef517ff9e97c69fef47a6cc06c5eb544d867fd0
James Dougan <jdplaysgames200@gmail.com>
2024-09-11 22:13:11 +0100

    create input validation pattern

--- jams/static/ts/global/endpoints_interfaces.ts
+++ jams/static/ts/global/endpoints_interfaces.ts
@@ -218,37 +218,6 @@ export interface RequestMultiModelJSONData extends EventLocation, EventTimeslot,
     force?:boolean
 }
 
-type SingularOrArray<T> = T | T[];
-type TransformToSingularOrArray<T> = {
-    [K in keyof T]: SingularOrArray<T[K]>;
-};
-
-export interface QueryStringData extends 
-    TransformToSingularOrArray<EventLocation>,
-    TransformToSingularOrArray<EventTimeslot>,
-    TransformToSingularOrArray<Location>,
-    TransformToSingularOrArray<Timeslot>,
-    TransformToSingularOrArray<Workshop>,
-    TransformToSingularOrArray<Session>,
-    TransformToSingularOrArray<DifficultyLevel>,
-    TransformToSingularOrArray<Event>,
-    TransformToSingularOrArray<User>,
-    TransformToSingularOrArray<Role>,
-    TransformToSingularOrArray<Page>,
-    TransformToSingularOrArray<PrivateAccessLog>,
-    TransformToSingularOrArray<VolunteerAttendance>,
-    TransformToSingularOrArray<WorkshopFile>,
-    TransformToSingularOrArray<FileData>,
-    TransformToSingularOrArray<FileVersion> {
-        $pagination_block_size?:number
-        $pagination_start_index?:number
-        $order_by?:string
-        $order_direction?:string
-        pre_induction_request?:boolean
-    }
-
-export type QueryStringKey = keyof QueryStringData;
-
 export interface BackendResponse<T> {
     message:string
     data:T
